49eb1ddaddb9bf574d78bfb83d784c63265ca78b
VitoRamos8 <vitorbramos88@gmail.com>
2024-10-10 19:34:41 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicação de teste 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aplicação_de_teste_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
